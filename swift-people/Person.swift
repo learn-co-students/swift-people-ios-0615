84785069086448358b35e76c355b5bd77bdbc365
AmitaiB <blickstein@gmail.com>
2015-12-20 14:34:58 -0500

    Done.

--- swift-people/Person.swift
+++ swift-people/Person.swift
@@ -12,7 +12,10 @@ class Person {
     let name:String
     var ageInYears:Int?
     var skills:[String]
-    var qualifiedTutor:Bool {return false}
+    var qualifiedTutor:Bool {
+        if skills.count >= 4 {return true}
+        else {return false}
+    }
     
     init(name:String, ageInYears:Int?) {
         self.name = name
@@ -28,13 +31,39 @@ class Person {
         self.init(name:"John Doe")
     }
     
+    
     func celebrateBirthday()->String {
-        if let age = ageInYears {
-            let ordinalAge = String(age) + age.ordinal().capitalizedString
-            return "HAPPY \(ordinalAge) BIRTHDAY, \(self.name.capitalizedString)!!!"
+        if let ageInYrs = ageInYears {
+            let age = ageInYrs + 1
+            ageInYears = Int?(age)
+            let ordinalAge = String(age) + age.ordinal().uppercaseString
+            return "HAPPY \(ordinalAge) BIRTHDAY, \(self.name.uppercaseString)!!!"
         }
         else {
-            return "HAPPY BIRTHDAY, \(self.name.capitalizedString)!!!"
+            return "HAPPY BIRTHDAY, \(self.name.uppercaseString)!!!"
         }
     }
+    
+    func addSkill(skill:String) {
+        var skillSet:Set = Set(self.skills)
+        skillSet.insert(skill)
+        self.skills = Array(skillSet)
+    }
+    
+    func learnSkillBash() {
+        addSkill("bash")
+    }
+    func learnSkillXcode()	{
+        addSkill("Xcode")
+    }
+    func learnSkillObjectiveC()	{
+        addSkill("Objective-C")
+    }
+    func learnSkillSwift() {
+        addSkill("Swift")
+    }
+    func learnSkillInterfaceBuilder() {
+        addSkill("Interface Builder")
+    }
+    
 }
